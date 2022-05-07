c9cf7145deb21583c00128634aa78eb750b90c97
sileam <46948165+sileam@users.noreply.github.com>
2022-05-07 16:21:23 +0800

    Add files via upload
    new

---
+++ python/callback/__init__.py
@@ -0,0 +1,3 @@
+from .CallBack import bind
+
+__all__ = ["bind"]
---
+++ python/main.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+# 打开游戏，登陆界面，
+#按H弹出help界面
+#imagemanager
+#config
+
+class main():
+
+
+    def __init__(self):
+        self.stateManager = None
+        self.imageManager = None
+        self.configManager = None
+        pass
